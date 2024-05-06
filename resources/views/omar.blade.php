c811e99b97af0d83daafcfe54eda87889bcf2a30
OmarDegel <137124151+OmarDegel@users.noreply.github.com>
2024-05-06 22:43:52 +0300

    el kasby

--- resources/views/omar.blade.php
+++ resources/views/omar.blade.php
@@ -20,17 +20,17 @@
 	<link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 
 
-	<link rel="stylesheet" href="fonts/icomoon/style.css">
-	<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+	<link rel="stylesheet" href={{ asset("fonts/icomoon/style.css")}}>
+	<link rel="stylesheet" href={{ asset("fonts/flaticon/font/flaticon.css")}}>
 
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 
-	<link rel="stylesheet" href="css/tiny-slider.css">
-	<link rel="stylesheet" href="css/aos.css">
-	<link rel="stylesheet" href="css/glightbox.min.css">
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href={{ asset("css/tiny-slider.css")}}>
+	<link rel="stylesheet" href={{ asset("css/aos.css")}}>
+	<link rel="stylesheet" href={{ asset("css/glightbox.min.css")}}>
+	<link rel="stylesheet" href={{ asset("css/style.css")}}>
 
-	<link rel="stylesheet" href="css/flatpickr.min.css">
+	<link rel="stylesheet" href={{ asset("css/flatpickr.min.css")}}>
 
 
 	<title>Blogy &mdash; Free Bootstrap 5 Website Template by Untree.co</title>
@@ -104,246 +104,19 @@
 	</nav>
 
 	<!-- Start retroy layout blog posts -->
-	<section class="section bg-light">
-		<div class="container">
-			<div class="row align-items-stretch retro-layout">
-				<div class="col-md-4">
-					<a href="single.html" class="h-entry mb-30 v-height gradient">
-
-						<div class="featured-img" style="background-image: url('images/img_2_horizontal.jpg');"></div>
-
-						<div class="text">
-							<span class="date">Apr. 14th, 2022</span>
-							<h2>AI can now kill those annoying cookie pop-ups</h2>
-						</div>
-					</a>
-					<a href="single.html" class="h-entry v-height gradient">
-
-						<div class="featured-img" style="background-image: url('images/img_5_horizontal.jpg');"></div>
-
-						<div class="text">
-							<span class="date">Apr. 14th, 2022</span>
-							<h2>Don’t assume your user data in the cloud is safe</h2>
-						</div>
-					</a>
-				</div>
-				<div class="col-md-4">
-					<a href="single.html" class="h-entry img-5 h-100 gradient">
-
-						<div class="featured-img" style="background-image: url('images/img_1_vertical.jpg');"></div>
-
-						<div class="text">
-							<span class="date">Apr. 14th, 2022</span>
-							<h2>Why is my internet so slow?</h2>
-						</div>
-					</a>
-				</div>
-				<div class="col-md-4">
-					<a href="single.html" class="h-entry mb-30 v-height gradient">
-
-						<div class="featured-img" style="background-image: url('images/img_3_horizontal.jpg');"></div>
-
-						<div class="text">
-							<span class="date">Apr. 14th, 2022</span>
-							<h2>Startup vs corporate: What job suits you best?</h2>
-						</div>
-					</a>
-					<a href="single.html" class="h-entry v-height gradient">
-
-						<div class="featured-img" style="background-image: url('images/img_4_horizontal.jpg');"></div>
-
-						<div class="text">
-							<span class="date">Apr. 14th, 2022</span>
-							<h2>Thought you loved Python? Wait until you meet Rust</h2>
-						</div>
-					</a>
-				</div>
-			</div>
-		</div>
-	</section>
+	
 	<!-- End retroy layout blog posts -->
 
 	<!-- Start posts-entry -->
-	<section class="section posts-entry">
-		<div class="container">
-			<div class="row mb-4">
-				<div class="col-sm-6">
-					<h2 class="posts-entry-title">Business</h2>
-				</div>
-				<div class="col-sm-6 text-sm-end"><a href="category.html" class="read-more">View All</a></div>
-			</div>
-			<div class="row g-3">
-				<div class="col-md-9">
-					<div class="row g-3">
-						<div class="col-md-6">
-							<div class="blog-entry">
-								<a href="single.html" class="img-link">
-									<img src="images/img_1_sq.jpg" alt="Image" class="img-fluid">
-								</a>
-								<span class="date">Apr. 14th, 2022</span>
-								<h2><a href="single.html">Thought you loved Python? Wait until you meet Rust</a></h2>
-								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis ea quis inventore vel voluptas.</p>
-								<p><a href="single.html" class="btn btn-sm btn-outline-primary">Read More</a></p>
-							</div>
-						</div>
-						<div class="col-md-6">
-							<div class="blog-entry">
-								<a href="single.html" class="img-link">
-									<img src="images/img_2_sq.jpg" alt="Image" class="img-fluid">
-								</a>
-								<span class="date">Apr. 14th, 2022</span>
-								<h2><a href="single.html">Startup vs corporate: What job suits you best?</a></h2>
-								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis ea quis inventore vel voluptas.</p>
-								<p><a href="single.html" class="btn btn-sm btn-outline-primary">Read More</a></p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-3">
-					<ul class="list-unstyled blog-entry-sm">
-						<li>
-							<span class="date">Apr. 14th, 2022</span>
-							<h3><a href="single.html">Don’t assume your user data in the cloud is safe</a></h3>
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis ea quis inventore vel voluptas.</p>
-							<p><a href="#" class="read-more">Continue Reading</a></p>
-						</li>
-
-						<li>
-							<span class="date">Apr. 14th, 2022</span>
-							<h3><a href="single.html">Meta unveils fees on metaverse sales</a></h3>
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis ea quis inventore vel voluptas.</p>
-							<p><a href="#" class="read-more">Continue Reading</a></p>
-						</li>
-
-						<li>
-							<span class="date">Apr. 14th, 2022</span>
-							<h3><a href="single.html">UK sees highest inflation in 30 years</a></h3>
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis ea quis inventore vel voluptas.</p>
-							<p><a href="#" class="read-more">Continue Reading</a></p>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</section>
+	
 	<!-- End posts-entry -->
 
 	<!-- Start posts-entry -->
-	<section class="section posts-entry posts-entry-sm bg-light">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-6 col-lg-3">
-					<div class="blog-entry">
-						<a href="single.html" class="img-link">
-							<img src="images/img_1_horizontal.jpg" alt="Image" class="img-fluid">
-						</a>
-						<span class="date">Apr. 14th, 2022</span>
-						<h2><a href="single.html">Thought you loved Python? Wait until you meet Rust</a></h2>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-						<p><a href="#" class="read-more">Continue Reading</a></p>
-					</div>
-				</div>
-				<div class="col-md-6 col-lg-3">
-					<div class="blog-entry">
-						<a href="single.html" class="img-link">
-							<img src="images/img_2_horizontal.jpg" alt="Image" class="img-fluid">
-						</a>
-						<span class="date">Apr. 14th, 2022</span>
-						<h2><a href="single.html">Startup vs corporate: What job suits you best?</a></h2>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-						<p><a href="#" class="read-more">Continue Reading</a></p>
-					</div>
-				</div>
-				<div class="col-md-6 col-lg-3">
-					<div class="blog-entry">
-						<a href="single.html" class="img-link">
-							<img src="images/img_3_horizontal.jpg" alt="Image" class="img-fluid">
-						</a>
-						<span class="date">Apr. 14th, 2022</span>
-						<h2><a href="single.html">UK sees highest inflation in 30 years</a></h2>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-						<p><a href="#" class="read-more">Continue Reading</a></p>
-					</div>
-				</div>
-				<div class="col-md-6 col-lg-3">
-					<div class="blog-entry">
-						<a href="single.html" class="img-link">
-							<img src="images/img_4_horizontal.jpg" alt="Image" class="img-fluid">
-						</a>
-						<span class="date">Apr. 14th, 2022</span>
-						<h2><a href="single.html">Don’t assume your user data in the cloud is safe</a></h2>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-						<p><a href="#" class="read-more">Continue Reading</a></p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+	
 	<!-- End posts-entry -->
 
 	<!-- Start posts-entry -->
-	<section class="section posts-entry">
-		<div class="container">
-			<div class="row mb-4">
-				<div class="col-sm-6">
-					<h2 class="posts-entry-title">Culture</h2>
-				</div>
-				<div class="col-sm-6 text-sm-end"><a href="category.html" class="read-more">View All</a></div>
-			</div>
-			<div class="row g-3">
-				<div class="col-md-9 order-md-2">
-					<div class="row g-3">
-						<div class="col-md-6">
-							<div class="blog-entry">
-								<a href="single.html" class="img-link">
-									<img src="images/img_1_sq.jpg" alt="Image" class="img-fluid">
-								</a>
-								<span class="date">Apr. 14th, 2022</span>
-								<h2><a href="single.html">Thought you loved Python? Wait until you meet Rust</a></h2>
-								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis ea quis inventore vel voluptas.</p>
-								<p><a href="single.html" class="btn btn-sm btn-outline-primary">Read More</a></p>
-							</div>
-						</div>
-						<div class="col-md-6">
-							<div class="blog-entry">
-								<a href="single.html" class="img-link">
-									<img src="images/img_2_sq.jpg" alt="Image" class="img-fluid">
-								</a>
-								<span class="date">Apr. 14th, 2022</span>
-								<h2><a href="single.html">Startup vs corporate: What job suits you best?</a></h2>
-								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis ea quis inventore vel voluptas.</p>
-								<p><a href="single.html" class="btn btn-sm btn-outline-primary">Read More</a></p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-3">
-					<ul class="list-unstyled blog-entry-sm">
-						<li>
-							<span class="date">Apr. 14th, 2022</span>
-							<h3><a href="single.html">Don’t assume your user data in the cloud is safe</a></h3>
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis ea quis inventore vel voluptas.</p>
-							<p><a href="#" class="read-more">Continue Reading</a></p>
-						</li>
-
-						<li>
-							<span class="date">Apr. 14th, 2022</span>
-							<h3><a href="single.html">Meta unveils fees on metaverse sales</a></h3>
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis ea quis inventore vel voluptas.</p>
-							<p><a href="#" class="read-more">Continue Reading</a></p>
-						</li>
-
-						<li>
-							<span class="date">Apr. 14th, 2022</span>
-							<h3><a href="single.html">UK sees highest inflation in 30 years</a></h3>
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis ea quis inventore vel voluptas.</p>
-							<p><a href="#" class="read-more">Continue Reading</a></p>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</section>
+	
 
 	<section class="section">
 		<div class="container">
@@ -528,60 +301,7 @@
 		</div>
 	</section>
 
-	<div class="section bg-light">
-		<div class="container">
-
-			<div class="row mb-4">
-				<div class="col-sm-6">
-					<h2 class="posts-entry-title">Travel</h2>
-				</div>
-				<div class="col-sm-6 text-sm-end"><a href="category.html" class="read-more">View All</a></div>
-			</div>
-
-			<div class="row align-items-stretch retro-layout-alt">
-
-				<div class="col-md-5 order-md-2">
-					<a href="single.html" class="hentry img-1 h-100 gradient">
-						<div class="featured-img" style="background-image: url('images/img_2_vertical.jpg');"></div>
-						<div class="text">
-							<span>February 12, 2019</span>
-							<h2>Meta unveils fees on metaverse sales</h2>
-						</div>
-					</a>
-				</div>
-
-				<div class="col-md-7">
-
-					<a href="single.html" class="hentry img-2 v-height mb30 gradient">
-						<div class="featured-img" style="background-image: url('images/img_1_horizontal.jpg');"></div>
-						<div class="text text-sm">
-							<span>February 12, 2019</span>
-							<h2>AI can now kill those annoying cookie pop-ups</h2>
-						</div>
-					</a>
-
-					<div class="two-col d-block d-md-flex justify-content-between">
-						<a href="single.html" class="hentry v-height img-2 gradient">
-							<div class="featured-img" style="background-image: url('images/img_2_sq.jpg');"></div>
-							<div class="text text-sm">
-								<span>February 12, 2019</span>
-								<h2>Don’t assume your user data in the cloud is safe</h2>
-							</div>
-						</a>
-						<a href="single.html" class="hentry v-height img-2 ms-auto float-end gradient">
-							<div class="featured-img" style="background-image: url('images/img_3_sq.jpg');"></div>
-							<div class="text text-sm">
-								<span>February 12, 2019</span>
-								<h2>Startup vs corporate: What job suits you best?</h2>
-							</div>
-						</a>
-					</div>  
-
-				</div>
-			</div>
-
-		</div>
-	</div>
+	
 
 
 	<footer class="site-footer">
@@ -696,17 +416,17 @@
     </div>
 
 
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/tiny-slider.js"></script>
+    <script src={{asset("js/bootstrap.bundle.min.js")}}></script>
+    <script src={{asset("js/tiny-slider.js")}}></script>
 
-    <script src="js/flatpickr.min.js"></script>
+    <script src={{asset("js/flatpickr.min.js")}}></script>
 
 
-    <script src="js/aos.js"></script>
-    <script src="js/glightbox.min.js"></script>
-    <script src="js/navbar.js"></script>
-    <script src="js/counter.js"></script>
-    <script src="js/custom.js"></script>
+    <script src={{asset("js/aos.js")}}></script>
+    <script src={{asset("js/glightbox.min.js")}}></script>
+    <script src={{asset("js/navbar.js")}}></script>
+    <script src={{asset("js/counter.js")}}></script>
+    <script src={{asset("js/custom.js")}}></script>
 
     
   </body>
