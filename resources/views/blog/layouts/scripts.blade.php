eef5c82272183c6014fa3d818c6faacdef6fc187
alkasaby <alkasaby@gmail.com>
2024-05-07 01:11:11 +0300

    git git

---
+++ resources/views/blog/layouts/scripts.blade.php
@@ -0,0 +1,15 @@
+<!-- Preloader -->
+<div id="overlayer"></div>
+<div class="loader">
+    <div class="spinner-border text-primary" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>
+</div>
+<script src={{ asset('js/bootstrap.bundle.min.js') }}></script>
+<script src={{ asset('js/tiny-slider.js') }}></script>
+<script src={{ asset('js/flatpickr.min.js') }}></script>
+<script src={{ asset('js/aos.js') }}></script>
+<script src={{ asset('js/glightbox.min.js') }}></script>
+<script src={{ asset('js/navbar.js') }}></script>
+<script src={{ asset('js/counter.js') }}></script>
+<script src={{ asset('js/custom.js') }}></script>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('omar');
+    return view('blog.master');
 });
 
 
